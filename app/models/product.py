e22d5332befa605de3bd6ad4fb0bd99208ebf746
Cathal <cathalpaz@gmail.com>
2023-08-07 20:37:35 -0400

    review model

--- app/models/product.py
+++ app/models/product.py
@@ -14,6 +14,7 @@ class Product(db.Model):
     description = db.Column(db.Text)
     seller_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     category = db.Column(db.String(20))
+    # AWS setup later
     image = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, default=datetime.utcnow())
     updated_at = db.Column(db.DateTime, default=datetime.utcnow())
@@ -28,3 +29,15 @@ class Product(db.Model):
     # relationship to cart
 
     # relationship to purchases
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'seller_id': self.seller_id,
+            'category': self.category,
+            'image': self.image
+        }
---
+++ app/models/review.py
@@ -0,0 +1,37 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('products.id')), nullable=False)
+    buyer_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    content = db.Column(db.Text)
+    rating = db.Column(db.Integer)
+    # AWS setup later
+    image = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+
+
+    # RELATIONSHIPS
+
+    # relationship to owner
+
+    # relationship to product
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'product_id': self.product_id,
+            'buyer_id': self.buyer_id,
+            'content': self.content,
+            'rating': self.rating,
+            'image': self.image
+        }
