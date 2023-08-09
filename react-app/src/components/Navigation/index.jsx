e5a616f78a04b55ea71ab83b3b1b427fdbeaff0f
Cathal <cathalpaz@gmail.com>
2023-08-09 01:20:11 -0400

    nav bar

--- react-app/src/components/Navigation/index.jsx
+++ react-app/src/components/Navigation/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import CategoryHeader from './CategoryHeader';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -14,50 +15,57 @@ function Navigation({ isLoaded }){
 
 
 	return (
-		<div className='nav__container'>
-			<NavLink exact to="/" className='nav__logo'>
-				<img src='/images/programazon.png'></img>
-			</NavLink>
-			<div className='nav__selling'>
-				<p>Selling items?</p>
-				{sessionUser ? <span>Click to start</span> : <span>Sign in to start</span>}
-			</div>
-			<div className='nav__search-bar'>
-				<div>All</div>
-				<input
-					className='search-input'
-					placeholder='Search Programazon'
-					// set up useStates
-				/>
-				<i class="fa-solid fa-magnifying-glass search-icon"></i>
-			</div>
-			<div className='nav__lang'>
-				* EN
-			</div>
-			{sessionUser ? (
+		<div className='nav__wrapper'>
+			<div className='nav__container'>
+				<NavLink exact to="/" className='nav__logo'>
+					<img src='/images/programazon.png'></img>
+				</NavLink>
+				<div className='nav__selling'>
+					<p>Selling items?</p>
+					{sessionUser ? <span>Click to start</span> : <span>Sign in to start</span>}
+				</div>
+				<div className='nav__search-bar'>
+					<div>All</div>
+					<input
+						className='search-input'
+						placeholder='Search Programazon'
+						// set up useStates
+					/>
+					<i class="fa-solid fa-magnifying-glass search-icon"></i>
+				</div>
+				<div className='nav__lang'>
+					<img src='/images/flag.png' />
+					<span>EN</span>
+				</div>
+				{sessionUser ? (
+					<div className='nav__box'>
+						<p>Hello, {sessionUser.username}</p>
+						<span>Account & Lists</span>
+					</div>
+				) : (
+					<div className='nav__box' onClick={handleMenu}>
+						<p>Hello, sign in</p>
+						<span>Account & Lists <i class="fa-solid fa-caret-down"></i></span>
+					</div>
+				)}
 				<div className='nav__box'>
-					<p>Hello, {sessionUser.username}</p>
-					<span>Account & Lists</span>
+					<p>Returns</p>
+					<span>& Orders</span>
 				</div>
-			) : (
-				<div className='nav__box' onClick={handleMenu}>
-					<p>Hello, sign in</p>
-					<span>Account & Lists <i class="fa-solid fa-caret-down"></i></span>
+				<div className='nav__cart'>
+					<p>0</p>
+					<span>Cart</span>
 				</div>
-			)}
-			<div className='nav__box'>
-				<p>Returns</p>
-				<span>& Orders</span>
-			</div>
-			<div className='nav__cart'>
-				Cart
-			</div>
 
-			{/* {isLoaded && (
-				<div>
-					<ProfileButton user={sessionUser} />
-				</div>
-			)} */}
+				{/* {isLoaded && (
+					<div>
+						<ProfileButton user={sessionUser} />
+					</div>
+				)} */}
+			</div>
+			<div className='nav__cat-header'>
+				<CategoryHeader />
+			</div>
 		</div>
 	);
 }
