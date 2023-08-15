8bf9ce31323dfb4fd274edbd7adc01b4b4b1f8ed
Cathal <cathalpaz@gmail.com>
2023-08-15 00:53:15 -0400

    product list page

--- react-app/src/components/UserProducts/index.jsx
+++ react-app/src/components/UserProducts/index.jsx
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { thunkGetUserProducts } from '../../store/products';
 import Loading from '../Loading'
 import dayjs from 'dayjs';
-
+import { useHistory } from 'react-router-dom'
+import './UserProducts.css'
 
 
 
 function UserProducts() {
+  const history = useHistory()
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
   const userProducts = useSelector(state => Object.values(state.products.userProducts))
@@ -21,29 +23,39 @@ function UserProducts() {
     <Loading />
   )
 
+  const sendToProduct = (id) => {
+    history.push(`/products/${id}`)
+  }
+
   return (
     <div className='user-products__container'>
-      <span>Your Products</span>
-      {userProducts.map(product => (
-        <div key={product.id} className='user-product'>
-          <div className='user-product__header'>
-            <h3>Listed on {dayjs(product.created_at).format('MMMM D, YYYY')}</h3>
-            <span>Category: {product.category}</span>
-          </div>
-          <div className='user-product__info'>
-            <img src={product.image} alt='product' />
-            <div className='user-product__middle'>
-              <span>{product.name}</span>
-              <span>Retail Price: ${product.price}</span>
-              <span>Remaining Stock: {product.stock_quantity}</span>
+      <div className='user-products__content'>
+        <span>Your Products</span>
+        {userProducts.map(product => (
+          <div key={product.id} className='user-product'>
+            <div className='user-product__header'>
+              <h3>Listed on {dayjs(product.created_at).format('MMMM D, YYYY')}</h3>
+              <span>Category: {product.category}</span>
             </div>
-            <button>Edit Product</button>
+            <div className='user-product__info'>
+              <div className='user-product__image'>
+                <img src={product.image} alt='product' />
+              </div>
+              <div className='user-product__middle'>
+                <span onClick={(() => sendToProduct(product.id))}>{product.name}</span>
+                <p>Retail Price: ${product.price}</p>
+                <p>Remaining Stock: {product.stock_quantity}</p>
+              </div>
+              <div className='user-product__edit'>
+                <button>Edit Product</button>
+              </div>
+            </div>
+            <div className='user-product__footer'>
+              <span>Delete Product</span>
+            </div>
           </div>
-          <div className='user-product__footer'>
-            <span>Delete Product</span>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
